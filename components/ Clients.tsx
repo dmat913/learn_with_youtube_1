f4ed2d785576582a66cb606b05502a22ffe1226d
dmat913 <soccer.82.d.y@gmail.com>
2024-07-13 22:22:33 +0900

    feat: created work experience

--- components/ Clients.tsx
+++ components/ Clients.tsx
@@ -7,7 +7,7 @@ function Clients() {
     <div className="py-20" id="testimonials">
       <h1 className="heading">
         Kind words from
-        <span className="text-purple">satisfied clients</span>
+        <span className="text-purple"> satisfied clients</span>
       </h1>
       <div className="flex flex-col items-center max-lg:mt-10">
         <InfiniteMovingCards
